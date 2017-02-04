788f3f6a9cbf10cdb191c56f4a08bbadb67b6cc3
Lionel Chen <lion6396643@hotmail.com>
2017-02-04 16:32:36 +0800

    a little modification

--- routes/apis.js
+++ routes/apis.js
@@ -6,100 +6,107 @@ var Blog = mongoose.model('Blog');
 var Contact = mongoose.model('Contact');
 var Comment = mongoose.model('Comment');
 
+
 /*logined   register*/
 router.post('/login', function(req, res, next) {
-	req.session.name = "aaa";
-	req.session.passwd = "bbb";	
-	if ((req.body.name != req.session.name)||(req.body.passwd != req.session.passwd)){
-		res.redirect('/');
-		return;
-	}
-	req.session.logined = true;
-	res.redirect('/');
+    
+    req.session.name = "aaa";
+    req.session.passwd = "bbb";
+    if ((req.body.name != req.session.name)||(req.body.passwd != req.session.passwd)){
+        res.redirect('/');
+        return;
+    }
+    req.session.logined = true;
+    res.redirect('/');
 });
+
+
 router.post('/contact', function(req, res, next){
-	new Contact({
-		Name: req.body.Name,
-		Contact: req.body.Contact,
-		Comment: req.body.Comment,
-		CreateDate: Date.now()
-	}).save(function(err){
-		if(err){
-			console.log("Fail to save to DB.\n");
-			return;
-		}
-		console.log("Save to DB.\n");
-	});
-	res.redirect("/");
+    new Contact({
+        Name: req.body.Name,
+        Contact: req.body.Contact,
+        Comment: req.body.Comment,
+        CreateDate: Date.now()
+    }).save(function(err){
+        if(err){
+            console.log("Fail to save to DB.\n");
+            return;
+        }
+        console.log("Save to DB.\n");
+    });
+    res.redirect("/");
 })
 
 /*add article*/
 router.post('/add', function(req, res, next) {
-	if (!req.session.name){
-		res.redirect('/');
-		return;
-	}
-	new Blog({
-		Classification: req.body.Classification,
-		Topic: req.body.Topic,
-		Article: req.body.Content,
-		CreateDate: Date.now()
-	}).save(function(err){
-		if(err){
-			console.log('Fail to save to DB.\n');
-			return;
-		}
-		console.log('Save to DB.\n');
-	});
-	res.redirect('/tutorial');		//can it refresh??
+    if (!req.session.name){
+        res.redirect('/');
+        return;
+    }
+    new Blog({
+        Classification: req.body.Classification,
+        Topic: req.body.Topic,
+        Article: req.body.Content,
+        CreateDate: Date.now()
+    }).save(function(err){
+        if(err){
+            console.log('Fail to save to DB.\n');
+            return;
+        }
+        console.log('Save to DB.\n');
+    });
+    res.redirect('/tutorial');        //can it refresh??
 });
 
 
 /*delete article*/
 router.get('/delete/:id', function(req, res, next) {
-	Blog.remove({_id:req.params.id }, function(err){		//params (look express 4)	http://expressjs.com/en/guide/routing.html#route-parameters
-		if(err){console.log("Fail to delete article.");}	//err=error?	which mean err is not NaN?
-		else{console.log("Done");}
-	});
-	res.redirect('/tutorial');
+    Blog.remove({_id:req.params.id }, function(err){        
+    //params (look express 4)    
+    //http://expressjs.com/en/guide/routing.html#route-parameters
+        if(err){console.log("Fail to delete article.");}
+        //err=error?    which mean err is not NaN?
+        else{console.log("Done");}
+    });
+    res.redirect('/tutorial');
 });
 
 /*update article*/
 router.post('/update/:id', function(req, res, next) {
-	if(!req.params.id){
-		res.redirect('/');
-		return;
-	}
-	Blog.update({_id:req.params.id},{	//指定文章
-		Classification: req.body.Classification,
-		Topic: req.body.Topic,
-		Article: req.body.Content    //時間不更新
-		},function(err){
-		if(err){console.log('Fail to update article.');}
-		else{console.log('Done')};
-	});
-	res.redirect('/tutorial');
+    if(!req.params.id){
+        res.redirect('/');
+        return;
+    }
+    Blog.update({_id:req.params.id},{
+        Classification: req.body.Classification,
+        Topic: req.body.Topic,
+        Article: req.body.Content    //time don''t update
+        },function(err){
+        if(err){console.log('Fail to update article.');}
+        else{console.log('Done')};
+    });
+    res.redirect('/tutorial');
 });
 
-/*  comment暫時沒有  */
+/*  comment not yet  */
 router.post('/comment/:id', function(req, res, next) {
-	if (!req.params.id){
-		res.redirect('/');
-		return;
-	}
-	new Comment({
-		Visitor: req.body.Visitor,
-		Comment: req.body.Comment,
-		MessageID: req.params.id,
-		CreateDate: Date.now()
-	}).save(function(err){
-		if(err){
-			console.log('Fail to save to DB.');
-			return;
-		}
-		else{console.log('Save to DB.');}
-	});
-	res.redirect('/users/message/'+req.params.id);
+    if (!req.params.id){
+        res.redirect('/');
+        return;
+    }
+    new Comment({
+        Visitor: req.body.Visitor,
+        Comment: req.body.Comment,
+        MessageID: req.params.id,
+        CreateDate: Date.now()
+    }).save(function(err){
+        if(err){
+            console.log('Fail to save to DB.');
+            return;
+        }
+        else{console.log('Save to DB.');}
+    });
+    res.redirect('/users/message/'+req.params.id);
 });
 
 module.exports = router;
